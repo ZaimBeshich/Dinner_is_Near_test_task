5463f32d7e2c9b409391c28983330804a77a0247
Zaim Beshich <zaimbeshich93@gmail.com>
2023-08-08 13:49:26 +0400

    add colors const

---
+++ src/constants/colors.js
@@ -0,0 +1,10 @@
+const COLORS = {
+  white: '#FFFFFF',
+  black: '#1E1E20',
+  grey: '#60626D',
+  grey_light: '#A9A9A9',
+  green: '#5CCA2D',
+  greenSecondary: '#65B443',
+  transparent: 'transparent',
+};
+export default COLORS;
